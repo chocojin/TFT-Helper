0a6cfe5c07a856d29f3e1f386738b9d7c75e2beb
JustinChCa <bluestars12221@gmail.com>
2019-07-19 18:35:02 -0400

    generic stuff

--- starter.py
+++ starter.py
@@ -1,8 +1,16 @@
-from Tkinter import *
+from tkinter import *
+
+class App:
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button1 = Button(frame, text="Quit", fg="red", command=frame.quit)
+        self.button1.pack(side=LEFT)
 
 root = Tk()
 
-w = Label(root, text="yo wat up")
-w.pack()
+app = App(root)
 
 root.mainloop()
